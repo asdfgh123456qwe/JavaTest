41c9c718ff81309f2c95d8a98b05c236b43bdd76
asdfg123456qwe <1090247102@qq.com>
2025-04-17 17:21:36 +0800

    Java基础练习项目提交

--- threadnew/src/main/java/org/threadnew/ThreadTest3.java
+++ threadnew/src/main/java/org/threadnew/ThreadTest3.java
@@ -1,5 +1,8 @@
 package org.threadnew;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * 掌握sleep方法，join方法的作用
  *
@@ -7,7 +10,28 @@ package org.threadnew;
  * @since 2025/4/17 16:46
  */
 public class ThreadTest3 {
-    public static void main(String[] args) {
+    private static final Logger log = LoggerFactory.getLogger(ThreadTest3.class);
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 1; i <= 5 ; i++) {
+            log.info("{}", i);
+            if (i == 3){
+                Thread.sleep(5000);
+            }
+        }
+
+
+        Thread t1 = new MyThread("1号线程");
+        t1.start();
+        t1.join();
+
+        Thread t2 = new MyThread("2号线程");
+        t2.start();
+        t2.join();
+
+        Thread t3 = new MyThread("3号线程");
+        t3.start();
+        t3.join();
 
     }
 }
